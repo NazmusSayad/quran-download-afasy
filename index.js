b2f536445aea3d9d3391b573ec1299a8bd7a5009
Nazmus Sayad <247sayad@gmail.com>
2022-08-15 05:36:07 +0600

    better

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ const download = require('download')
 const { readFileSync, writeFileSync } = require('fs')
 
 const file = './verses-ogg.json'
-
 const Links = JSON.parse(readFileSync(file, 'utf-8'))
+
 ;(async () => {
-  for (let verseLink of Links) {
-    const verseDetails = verseLink.split('/')
-    const format = verseDetails.at(-2)
-    const number = verseDetails.at(-1).slice(0, 3)
+  try {
+    for (let verseLink of Links) {
+      const verseDetails = verseLink.split('/')
+      const format = verseDetails.at(-2)
+      const number = verseDetails.at(-1).slice(0, 3)
 
-    await download(verseLink, `download/${format}/${number}`)
+      await download(verseLink, `download/${format}/${number}`)
 
-    Links.shift()
+      Links.shift()
+      console.log(verseLink)
+    }
+  } catch {
     writeFileSync(file, JSON.stringify(Links))
-    console.log(verseLink)
   }
 })()
