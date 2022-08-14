5606f6e1253d4f4907bb33f64c4632db9c5f6160
Nazmus Sayad <247sayad@gmail.com>
2022-08-15 05:41:19 +0600

    bug fixed!

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ const download = require('download')
 const { readFileSync, writeFileSync } = require('fs')
 
 const file = './verses-ogg.json'
-const Links = JSON.parse(readFileSync(file, 'utf-8'))
+const allLinks = JSON.parse(readFileSync(file, 'utf-8'))
+const TEMP_LINKS = [...allLinks]
 
 ;(async () => {
   try {
-    for (let verseLink of Links) {
+    for (let verseLink of allLinks) {
       const verseDetails = verseLink.split('/')
       const format = verseDetails.at(-2)
       const number = verseDetails.at(-1).slice(0, 3)
 
       await download(verseLink, `download/${format}/${number}`)
 
-      Links.shift()
+      TEMP_LINKS.shift()
       console.log(verseLink)
     }
   } catch {
-    writeFileSync(file, JSON.stringify(Links))
+    writeFileSync(file, JSON.stringify(TEMP_LINKS))
   }
 })()
